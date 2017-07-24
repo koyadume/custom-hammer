c5fab9befa4db02c0534e4ca386ab704b7c2923f
Shailendra Singh <shailendra_01@outlook.com>
2017-07-24 10:35:21 +0530

    Misc updates.

--- hammer/src/main/java/in/mechcloud/piston/apps/hammer/actions/HelloPluginAction.java
+++ hammer/src/main/java/in/mechcloud/piston/apps/hammer/actions/HelloPluginAction.java
@@ -2,6 +2,7 @@ package in.mechcloud.piston.apps.hammer.actions;
 
 import in.koyad.piston.app.api.annotation.AnnoPluginAction;
 import in.koyad.piston.app.api.model.Request;
+import in.koyad.piston.app.api.model.Response;
 import in.koyad.piston.app.api.plugin.BasePluginAction;
 import in.koyad.piston.common.basic.exception.FrameworkException;
 
@@ -13,7 +14,7 @@ public class HelloPluginAction extends BasePluginAction {
 	public static final String ACTION_NAME = "hello";
 
 	@Override
-	public String execute(Request arg0) throws FrameworkException {
+	public String execute(Request req, Response resp) throws FrameworkException {
 		return "/hello.xml";
 	}
 
